9451ef2cc360c31355c5e339756f70d6bebb5df0
Marcos M. Junior <marcos.mth.jr@gmail.com.br>
2021-05-21 05:25:38 -0300

    Chapter 3 finalizado

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCase/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCase/updateUserAvatar/UpdateUserAvatarController";
 
 const userRoutes = Router();
 
-const createUserController = new CreateUserController();
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
+const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 userRoutes.post("/", createUserController.handle);
+
+userRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { userRoutes };
